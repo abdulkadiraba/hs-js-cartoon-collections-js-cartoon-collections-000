0ef112acc064b065fda2b90636b75f52c2488879
Abdulkadir Aba <abdulkadir.aba786@gmail.com>
2019-10-16 05:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function dwarfRollCall(dwarves) {
+function dwarfrollcall(dwarves){ 
+  dwarveslist = ""
+  for (var i=0; i<dwarves.length; i++) {
+    dwarveslist += i+1 + "." + dwarves[i] + " "
+  }
+  return dwarveslist 
 }
-
 function summonCaptainPlanet(planeteerCalls){
+  for (var i=0; i<planeteerCalls.length; i++) {
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!" 
+  } 
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (var i=0,
 }
 
 function findTheCheese (foods) {
-}
+  cheeses = ["cheddar", "gouda" , "camembert"]
+  for (var i = 0; i < foods.length; i++) {
+    if(cheeses.includes(foods[i])){
+      return true
+    }
+  }
+  return false 
+} 
+
+
+a = ["Ahmed", "Diro", "Osman"]
+console.log(summonCaptainPlanet(a))
